e49e681933fed449862791f6eb29749a7fa05129
Beldar84 <sedano.jon@gmail.com>
2026-01-23 19:04:17 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // JW Timeline Service Worker
 // Enables offline functionality by caching essential resources
 
-const CACHE_NAME = 'jw-timeline-v8';
+const CACHE_NAME = 'jw-timeline-v9';
 
 // Resources to cache immediately on install
 const STATIC_RESOURCES = [
